1e8d1069ce4709e66821c1b1c07d81fff2160c7a
이창우 <cwadven@naver.com>
2021-11-14 00:20:57 +0900

    chore: APPEND_SLASH 제거
    - RESTAPI 규격 맞추기

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,12 +10,12 @@ from rest_auth.views import (
 
 urlpatterns = [
     # 회원가입
-    path("registration", RegisterView.as_view(), name='rest_register'),
+    path("/registration", RegisterView.as_view(), name='rest_register'),
     # 로그인, 로그아웃
-    path('login', LoginView.as_view(), name='rest_login'),
-    path('logout', LogoutView.as_view(), name='rest_logout'),
+    path('/login', LoginView.as_view(), name='rest_login'),
+    path('/logout', LogoutView.as_view(), name='rest_logout'),
     # 비밀번호 수정
-    path('password-change', PasswordChangeView.as_view(), name='rest_password_change'),
+    path('/password-change', PasswordChangeView.as_view(), name='rest_password_change'),
 
     # 비밀번호 찾기
     # # 이메일 전송 가능하게 해야함
@@ -24,7 +24,7 @@ urlpatterns = [
     # path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
     # 이메일 인증하기
-    path('allauth/', include('allauth.urls')),
+    path('/allauth', include('allauth.urls')),
     # 이메일 인증
     # re_path(r'^account-confirm/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     # re_path(r'^account-confirm/(?P<key>[-:\w]+)$', ConfirmEmailView.as_view(), name='account_confirm_email'),
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,11 +6,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
     # 회원
-    path('accounts/', include('accounts.urls')),
-    path('roadmap/', include('roadmap.urls')),
+    path('accounts', include('accounts.urls')),
+    path('roadmap', include('roadmap.urls')),
+
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
