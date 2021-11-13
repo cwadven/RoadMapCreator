5ccbcdf85dc81e25e9c389aa21d9112932951f70
이창우 <cwadven@naver.com>
2021-11-13 23:52:26 +0900

    chore: migrations, model 수정
    - middle table wieght 속성 추가로 직접 Model 정의

--- roadmap/admin.py
+++ roadmap/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from roadmap.models import RoadMap, BaseNode, UserNode
+from roadmap.models import RoadMap, BaseNode, UserNode, BaseNodeDegree
 
 admin.site.register(RoadMap)
 admin.site.register(BaseNode)
 admin.site.register(UserNode)
-
+admin.site.register(BaseNodeDegree)
 
--- roadmap/models.py
+++ roadmap/models.py
@@ -21,8 +21,7 @@ class BaseNode(models.Model):
     roadmap = models.ForeignKey(RoadMap, on_delete=models.CASCADE)
     name = models.CharField(max_length=64, db_index=True)
     description = models.TextField(null=True)
-    in_degree = models.ManyToManyField("self", through="BaseNodeInDegree", symmetrical=False, related_name="income_degree", null=True, blank=True)
-    out_degree = models.ManyToManyField("self", through="BaseNodeOutDegree", symmetrical=False, related_name="outcome_degree", null=True, blank=True)
+    degree = models.ManyToManyField("self", through="BaseNodeDegree", symmetrical=False, null=True, blank=True)
     difficulty = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     display_level = models.PositiveIntegerField()
     hex_color_code = models.CharField(max_length=9)
@@ -45,13 +44,10 @@ class UserNode(models.Model):
         return f"{self.account} {self.active_node}"
 
 
-class BaseNodeInDegree(models.Model):
-    from_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='in_degree_from')
-    to_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='in_degree_to')
+class BaseNodeDegree(models.Model):
+    from_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='degree_from')
+    to_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='degree_to')
     weight = models.IntegerField(default=0)
 
-
-class BaseNodeOutDegree(models.Model):
-    from_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='out_degree_from')
-    to_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='out_degree_to')
-    weight = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.from_basenode} -> {self.to_basenode}"
