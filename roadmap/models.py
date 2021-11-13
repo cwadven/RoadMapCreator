cc33109306c29383aa8f65a7529d82215d6e0c6b
이창우 <cwadven@naver.com>
2021-11-13 23:33:05 +0900

    chore: migrations, model 수정
    - middle table wieght 속성 추가로 직접 Model 정의

--- roadmap/models.py
+++ roadmap/models.py
@@ -21,8 +21,8 @@ class BaseNode(models.Model):
     roadmap = models.ForeignKey(RoadMap, on_delete=models.CASCADE)
     name = models.CharField(max_length=64, db_index=True)
     description = models.TextField(null=True)
-    in_degree = models.ManyToManyField("self", symmetrical=False, related_name="income_degree", null=True, blank=True)
-    out_degree = models.ManyToManyField("self", symmetrical=False, related_name="outcome_degree", null=True, blank=True)
+    in_degree = models.ManyToManyField("self", through="BaseNodeInDegree", symmetrical=False, related_name="income_degree", null=True, blank=True)
+    out_degree = models.ManyToManyField("self", through="BaseNodeOutDegree", symmetrical=False, related_name="outcome_degree", null=True, blank=True)
     difficulty = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     display_level = models.PositiveIntegerField()
     hex_color_code = models.CharField(max_length=9)
@@ -45,3 +45,13 @@ class UserNode(models.Model):
         return f"{self.account} {self.active_node}"
 
 
+class BaseNodeInDegree(models.Model):
+    from_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='in_degree_from')
+    to_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='in_degree_to')
+    weight = models.IntegerField(default=0)
+
+
+class BaseNodeOutDegree(models.Model):
+    from_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='out_degree_from')
+    to_basenode = models.ForeignKey(BaseNode, on_delete=models.CASCADE, related_name='out_degree_to')
+    weight = models.IntegerField(default=0)
