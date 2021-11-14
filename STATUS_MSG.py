989583099ce6b732f7a7abdab070ec8dc729d363
이창우 <cwadven@naver.com>
2021-11-15 00:06:02 +0900

    feat: start_basenode_id 와 end_basenode_id 상태
    - start_basenode_id 와 end_basenode_id 연결되지 않은 에러 문구

--- STATUS_MSG.py
+++ STATUS_MSG.py
@@ -1,3 +1,4 @@
 MSG_LOGIN_REQUIRED = "login required"
 MSG_UNAUTHORIZED = "unauthorized"
 MSG_DIFFERENT_ROADMAP = "each 'from' and 'to' BaseNode of RoadMap is different"
+MSG_NOT_CONNECTED = "'from' and 'to' BaseNode is not connected"
