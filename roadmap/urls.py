bd08bd5d49694f7cff9e585af7eb7963d9e8ea0d
이창우 <cwadven@naver.com>
2021-11-14 18:40:09 +0900

    feat: RoadMap Detail 조회
    - 로드맵 조회
    - BaseNode 와 그 BaseNode 가 누가 연결되어있는지 확인

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from roadmap.views import RoadMapAPI
+from roadmap.views import RoadMapAPI, RoadMapDetailAPI
 
 urlpatterns = [
     path("", RoadMapAPI.as_view()),
+    path("/<int:id>", RoadMapDetailAPI.as_view()),
 ]
--- roadmap/views.py
+++ roadmap/views.py
@@ -1,26 +1,22 @@
-from django.db.models import F
+from django.db.models import F, Prefetch
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from common_library import mandatory_key, optional_key
-from roadmap.models import RoadMap
+from roadmap.models import RoadMap, BaseNode
+from roadmap.serializers import RoadMapListSerializer, RoadMapDetailSerializer
 
 
 class RoadMapAPI(APIView):
     def get(self, request):
-        roadmap_set = RoadMap.objects.annotate(
-            username=F("account__username")
-        ).values(
-            "id",
-            "username",
-            "title",
-            "description",
-            "image",
-            "created_at",
-            "updated_at",
+        roadmap_set = RoadMap.objects.select_related(
+            'account'
+        ).all()
+        return Response(
+            data={"roadmap_set": RoadMapListSerializer(roadmap_set, many=True).data},
+            status=status.HTTP_200_OK
         )
-        return Response(data={"roadmap_set": roadmap_set}, status=status.HTTP_200_OK)
 
     def post(self, request):
         if not request.user.is_authenticated:
@@ -38,3 +34,22 @@ class RoadMapAPI(APIView):
         )
 
         return Response(data={"success": roadmap.id}, status=status.HTTP_200_OK)
+
+
+class RoadMapDetailAPI(APIView):
+    def get(self, request, id):
+        try:
+            roadmap = RoadMap.objects.prefetch_related(
+                Prefetch(
+                    "basenode_set",
+                    queryset=BaseNode.objects.prefetch_related("degree_from").all()
+                ),
+            ).annotate(
+                username=F("account__username"),
+            ).get(
+                id=id
+            )
+        except RoadMap.DoesNotExist:
+            return Response(data={"message": "no such roadmap"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(data={"roadmap": RoadMapDetailSerializer(roadmap).data}, status=status.HTTP_200_OK)
