24f168ad50d48c8cdba5a62c68715a9198f00a42
이창우 <cwadven@naver.com>
2021-11-14 19:10:05 +0900

    feat: RoadMap PUT
    - RoadMap 정보 수정하는 API 구현

--- roadmap/views.py
+++ roadmap/views.py
@@ -1,8 +1,10 @@
+from django.db import transaction
 from django.db.models import F, Prefetch
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from STATUS_MSG import MSG_UNAUTHORIZED, MSG_LOGIN_REQUIRED
 from common_library import mandatory_key, optional_key
 from roadmap.models import RoadMap, BaseNode
 from roadmap.serializers import RoadMapListSerializer, RoadMapDetailSerializer
@@ -20,7 +22,7 @@ class RoadMapAPI(APIView):
 
     def post(self, request):
         if not request.user.is_authenticated:
-            return Response(data={"message": "login required"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"error": MSG_LOGIN_REQUIRED}, status=status.HTTP_400_BAD_REQUEST)
 
         title = mandatory_key(request, "title")
         description = optional_key(request, "description")
@@ -49,7 +51,50 @@ class RoadMapDetailAPI(APIView):
             ).get(
                 id=id
             )
-        except RoadMap.DoesNotExist:
-            return Response(data={"message": "no such roadmap"}, status=status.HTTP_400_BAD_REQUEST)
+        except RoadMap.DoesNotExist as e:
+            return Response(data={"error": e}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(data={"roadmap": RoadMapDetailSerializer(roadmap).data}, status=status.HTTP_200_OK)
+        return Response(data={"success": RoadMapDetailSerializer(roadmap).data}, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        if not request.user.is_authenticated:
+            return Response(data={"error": MSG_LOGIN_REQUIRED}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            roadmap = RoadMap.objects.prefetch_related(
+                Prefetch(
+                    "basenode_set",
+                    queryset=BaseNode.objects.prefetch_related("degree_from").all()
+                ),
+            ).annotate(
+                username=F("account__username"),
+            ).get(
+                id=id
+            )
+
+            if request.user != roadmap.account:
+                return Response(data={"error": MSG_UNAUTHORIZED}, status=status.HTTP_400_BAD_REQUEST)
+
+            update_fields = ["updated_at"]
+
+            with transaction.atomic():
+                title = optional_key(request, "title")
+                description = optional_key(request, "description")
+                image = request.FILES.get("image")
+
+                if title:
+                    roadmap.title = title
+                    update_fields.append("title")
+                if description:
+                    roadmap.description = description
+                    update_fields.append("description")
+                if image:
+                    roadmap.image = image
+                    update_fields.append("image")
+
+                roadmap.save(update_fields=update_fields)
+
+            return Response(data={"success": RoadMapDetailSerializer(roadmap).data}, status=status.HTTP_200_OK)
+
+        except RoadMap.DoesNotExist as e:
+            return Response(data={"error": e}, status=status.HTTP_400_BAD_REQUEST)
