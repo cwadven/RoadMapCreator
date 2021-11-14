ed281efaaf269bc7edbb591f5e95fe40e62e41d1
이창우 <cwadven@naver.com>
2021-11-14 18:39:20 +0900

    feat: Serializer 생성
    - RoadMap, BaseNode, BaseNodeDegree 를 위한 Serializer 생성

---
+++ roadmap/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+
+from roadmap.models import RoadMap, BaseNode, BaseNodeDegree
+
+
+class BaseNodeDegreeDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BaseNodeDegree
+        fields = [
+            "to_basenode_id",
+            "weight"
+        ]
+
+
+class BaseNodeDetailSerializer(serializers.ModelSerializer):
+    degree_set = BaseNodeDegreeDetailSerializer(source="degree_from", read_only=True, many=True)
+
+    class Meta:
+        model = BaseNode
+        fields = [
+            "id",
+            "name",
+            "description",
+            "difficulty",
+            "display_level",
+            "hex_color_code",
+            "active_count",
+            "image",
+            "created_at",
+            "updated_at",
+            "degree_set",
+        ]
+
+
+class RoadMapListSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="account.username")
+
+    class Meta:
+        model = RoadMap
+        fields = [
+            "id",
+            "username",
+            "title",
+            "description",
+            "image",
+            "created_at",
+        ]
+
+
+class RoadMapDetailSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="account.username")
+    basenode_set = BaseNodeDetailSerializer(many=True)
+
+    class Meta:
+        model = RoadMap
+        fields = [
+            "id",
+            "username",
+            "title",
+            "description",
+            "image",
+            "created_at",
+            "basenode_set",
+        ]
+
+
