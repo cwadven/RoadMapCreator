a6b4daa62798adb7a0e9ebf32efc0f0ca48c083a
이창우 <cwadven@naver.com>
2021-11-21 21:56:38 +0900

    feat: Styled Component 고도화
    - 디자인 고도화

--- web/project/roadmapcreator/src/Component/Header.js
+++ web/project/roadmapcreator/src/Component/Header.js
@@ -1,31 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {withRouter} from "CustomHock/withRouter";
+
+const Container = styled.header`
+    color: while;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px;
+    background-color: rgba(20, 20, 20, 0.8);
+    box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
 const List = styled.ul`
     display: flex;
-    &:hover {
-        background-color: blue;
-    }
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+    width: 130px;
+    height: 50px;
+    text-align: center;
+    border-bottom: 5px solid
+        ${props => (props.current ? '#3498db' : 'transparent')};
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header>
+        <Container>
             <List>
-                <Item>
-                    <SLink to={'/'}>Home</SLink>
+                <Item current={window.location.pathname === '/'}>
+                    <SLink to={'/'}>메인페이지</SLink>
                 </Item>
-                <Item>
-                    <SLink to={'/roadmap'}>RoadMap</SLink>
+                <Item current={window.location.pathname === '/roadmap'}>
+                    <SLink to={'/roadmap'}>로드맵</SLink>
                 </Item>
             </List>
-        </header>
+        </Container>
     );
 }
 
-export default Header;
+export default withRouter(Header);
