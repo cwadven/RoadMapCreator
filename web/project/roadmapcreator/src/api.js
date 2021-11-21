7376b3a0d313199db3afc7e159cdf83f40267890
이창우 <cwadven@naver.com>
2021-11-21 22:40:44 +0900

    feat: axios api request 만들기
    - request 요청 생성

---
+++ web/project/roadmapcreator/src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',
+    // params: {
+    //     api_key: 'a4a599b5b19fb6fca6b94df7abe615d0',
+    //     language: 'en-US',
+    // },
+});
+
+export const roadMap = {
+    roadMapList: () => api.get('/roadmap'),
+    roadMapDetail: (roadMapId) => api.get(`/roadmap/${roadMapId}`),
+    findShortestPath: (roadMapId) => api.get(`/roadmap/${roadMapId}/shortest-direction`, {}),
+};
+
+export const baseNode = {
+    createBaseNode: (roadMapId) => api.post(`/roadmap/${roadMapId}/basenode`, {}),
+    updateBaseNode: (baseNodeId) => api.put(`/roadmap/basenode/${baseNodeId}`, {}),
+};
+
+export const degree = {
+    createOrUpdateDegree: () => api.post('/roadmap/basenode/degree', {}),
+    deleteDegree: () => api.put('/roadmap/basenode/degree', {}),
+};
