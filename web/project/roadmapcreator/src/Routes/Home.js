eb4031618b450ae67a22fd99f84ea45e36379403
이창우 <cwadven@naver.com>
2021-11-21 22:57:13 +0900

    feat: 데이터 가져오기
    - 메인페이지에 Axios 로 데이터 요청하여 가져오기

--- web/project/roadmapcreator/src/Routes/Home.js
+++ web/project/roadmapcreator/src/Routes/Home.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {roadMap} from "api";
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
 
-const Home = (props) => {
+const RoadMapItem = styled.div`
+    margin-top: 20px;
+`;
+
+const Home = () => {
+    const [roadMapSet, setRoadMapSet] = useState([]);
+
+    useEffect(async () => {
+        const {data: {roadmap_set}} = await roadMap.roadMapList();
+        setRoadMapSet(roadmap_set);
+    }, [])
+
     return (
-        <div>Hello</div>
+        <div>
+            {roadMapSet.length ? roadMapSet.map((data) => {
+                return (
+                    <RoadMapItem key={data.id}>
+                        <div>순번: {data.id}</div>
+                        <div>작성자: {data.username}</div>
+                        <div>제목: {data.title}</div>
+                        <div>설명: {data.description}</div>
+                        <div>이미지: {data.image}</div>
+                        <div>생성일: {new Date(data.created_at).toLocaleString()}</div>
+                        <Link to={`/roadmap/${data.id}`}>자세히보기🎈</Link>
+                    </RoadMapItem>
+                )
+            }) : null}
+        </div>
     );
 }
 
