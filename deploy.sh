3cf8244721c416cb33337e9c72cd11858f0493a0
cwadven <cwadven@naver.com>
2021-12-09 12:17:03 +0000

    deploy source 대신

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cd /var/www/roadmap/web/project/roadmapcreator && yarn build 2>&1
-./var/www/roadmap/venv/bin/activate && python /var/www/roadmap/manage.py collectstatic --noinput
+. /var/www/roadmap/venv/bin/activate && python /var/www/roadmap/manage.py collectstatic --noinput
 systemctl restart nginx
 systemctl restart uwsgi
