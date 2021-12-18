60a5193b8408e4e94792eebce13dff708e99fdd5
cwadven <cwadven@naver.com>
2021-12-18 21:28:20 +0900

    feat: 만약 폴더가 없을 경우 직접 생성해 주기
    - 처음 배포할 때, 폴더가 없을 경우 생성해주기

--- web/project/roadmapcreator/build_copy.js
+++ web/project/roadmapcreator/build_copy.js
@@ -3,18 +3,27 @@ const ncp = require('ncp').ncp;
 
 ncp.limit = 16;
 
+if (!fs.existsSync("../../static/static/css")){
+    fs.mkdirSync("../../static/static/css");
+}
 fs.rmdirSync("../../static/static/css", {recursive: true}, function (err) {
     if (err) {
         return console.log(err);
     }
     console.log('Legacy CSS Remove')
 })
+if (!fs.existsSync("../../static/static/js")){
+    fs.mkdirSync("../../static/static/js");
+}
 fs.rmdirSync("../../static/static/js", {recursive: true}, function (err) {
     if (err) {
         return console.log(err);
     }
     console.log('Legacy JS Remove')
 })
+if (!fs.existsSync("../../static/static/media")){
+    fs.mkdirSync("../../static/static/media");
+}
 fs.rmdirSync("../../static/static/media", {recursive: true}, function (err) {
     if (err) {
         return console.log(err);
@@ -23,6 +32,10 @@ fs.rmdirSync("../../static/static/media", {recursive: true}, function (err) {
 })
 
 console.log('Copying files...');
+
+if (!fs.existsSync("../../templates/")){
+    fs.mkdirSync("../../templates/");
+}
 ncp('build/index.html', '../../templates/index.html', function (err) {
     if (err) {
         return console.error(err);
